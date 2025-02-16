029f6d5cd2617ad611df7324a8bcf628d4abf5df
wkeyflixzyy <ivanmynkow@gmail.com>
2025-02-16 16:52:36 +0200

    new game candy crush

--- game.js
+++ game.js
@@ -1,41 +1,63 @@
 
 // Suzdavame promenlivi
-let stypkaVrag=[], brVragove, towerX=[], towerY=[], brTowers, paveY=[], paveX=[];
+let tipVPole=[], razmerX, razmerY, kvPx, brTipove, klX, klY, br, br1;
 
 function init() {
-    brVragove=70;
-    brTowers=0;
-    for(let v=0;v<brVragove; v++){
-        stypkaVrag[v]=v*(-randomInteger(200));
-    }  
+    //Kodut tuk se izpulnqva vednuj v nachaloto
+    razmerX=6;
+    razmerY=6;
+    kvPx=600/razmerY;
+    brTipove=3;
+    for(let x=0; x<razmerX; x++){
+        tipVPole[x]=[]
+        for(let y=0; y<razmerY; y++){
+            tipVPole[x][y]=randomInteger(brTipove);
+        }
+    }
 }
 
 function update() {
-    for(let v=0;v<brVragove; v++){
-        stypkaVrag[v]=stypkaVrag[v]+1;
-    }
+    //Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
+   
 }
 
 function draw() {
     // Tuk naprogramirai kakvo da se risuva
-    for(let v=0;v<brVragove; v++){
-        stypkaVrag[v]=v*(-randomInteger(50));
-    }
-
-    for(let t=0; t<brTowers;t++){
-        drawImage(house[0], towerX[t], towerY[t], 50, 50);
-    }
-    for(let v=0; v<brVragove; v++){
-        drawImage(tank[2], towerX[v], towerY[v], 50, 50);
+    drawImage(backField, 0, 0, 800, 600);
+    for(let x=0; x<razmerX; x++){
+        for(let y=0; y<razmerY; y++){
+            for(let k=0; k<brTipove; k++){
+                if(tipVPole[x][y]==k){
+                    drawImage(gem[k], x*kvPx, y*kvPx, kvPx, kvPx );
+                }
+            }
+            /*if(tipVPole[x][y]==0){
+                drawImage(gem[1],x*kvPx, y*kvPx, kvPx, kvPx);
+            }
+            if(tipVPole[x][y]==1){
+                drawImage(gem[2], x*kvPx, y*kvPx, kvPx, kvPx);
+            }
+            if(tipVPole[x][y]==2){
+                drawImage(gem[3], x*kvPx, y*kvPx, kvPx, kvPx);
+            } */
+        }
     }
 }
 
 function mouseup() {
-    // Pri klik s lqv buton - pokaji koordinatite na mishkata
-    console.log("Mouse clicked at", mouseX, mouseY);
-    towerX[brTowers]=mouseX;
-    towerY[brTowers]=mouseY;
-    brTowers++;
+    klX=Math.floor(mouseX/kvPx);
+    klY=Math.floor(mouseY/kvPx);
+    console.log("Red=", klY, "Kolona=", klX);
+    br=0;
+    br1=0;
+    while(klX+br<razmerX && tipVPole[klX+br][klY]==tipVPole[klX][klY]){
+        br++;
+    }
+    while(klX-br1>=0 && tipVPole[klX-br1][klY]==tipVPole[klX][klY]){
+        br1++;
+    }
+    console.log("Broi ednakvi=", br);
+    console.log("Broi ednakvi=", br1);
 }
 
 function keyup(key) {
