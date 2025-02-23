465bbad0a230b6ddcbfb22c4b3e339f06ea61c66
wkeyflixzyy <ivanmynkow@gmail.com>
2025-02-23 12:07:42 +0200

    candy_crush nivo2

--- game.js
+++ game.js
@@ -1,24 +1,42 @@
 
 // Suzdavame promenlivi
-let tipVPole=[], razmerX, razmerY, kvPx, brTipove, klX, klY, br, br1;
-
+let tipVPole=[], razmerX, razmerY, kvPx, brTipove, klX, klY, br, br1, brtochki;
+let nivo2=false;
 function init() {
     //Kodut tuk se izpulnqva vednuj v nachaloto
     razmerX=6;
     razmerY=6;
     kvPx=600/razmerY;
     brTipove=3;
+    brtochki=0;
     for(let x=0; x<razmerX; x++){
         tipVPole[x]=[]
         for(let y=0; y<razmerY; y++){
-            tipVPole[x][y]=randomInteger(brTipove);
+            tipVPole[x][y]=1+randomInteger(brTipove);
         }
     }
 }
-
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 10000000; i=i+1) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 function update() {
-    //Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
-   
+    sleep(200)
+    for(let x=0; x<razmerX; x++){
+        if(tipVPole[x][0]==0){
+            tipVPole[x][0]=1+randomInteger(brTipove);
+        }
+        for(let y=1; y<razmerY; y++){
+            if(tipVPole[x][y]==0){
+                tipVPole[x][y]=tipVPole[x][y-1];
+                tipVPole[x][y-1]=0;
+            }
+        }
+    }
 }
 
 function draw() {
@@ -26,7 +44,7 @@ function draw() {
     drawImage(backField, 0, 0, 800, 600);
     for(let x=0; x<razmerX; x++){
         for(let y=0; y<razmerY; y++){
-            for(let k=0; k<brTipove; k++){
+            for(let k=0; k<brTipove+1; k++){
                 if(tipVPole[x][y]==k){
                     drawImage(gem[k], x*kvPx, y*kvPx, kvPx, kvPx );
                 }
@@ -42,8 +60,12 @@ function draw() {
             } */
         }
     }
-}
 
+    context.fillStyle="blue";
+    context.font="40px Elephant";
+    context.fillText("Tochki", 650, 150);
+    context.fillText(brtochki, 650, 200);
+}
 function mouseup() {
     klX=Math.floor(mouseX/kvPx);
     klY=Math.floor(mouseY/kvPx);
@@ -52,6 +74,23 @@ function mouseup() {
     br1=0;
     while(klX+br<razmerX && tipVPole[klX+br][klY]==tipVPole[klX][klY]){
         br++;
+    }
+    if(br>1){
+        brtochki=brtochki+br;
+        for(let d=0; d<br; d++){
+        tipVPole[klX+d][klY]=0;}
+    }
+    if(brtochki>30 && nivo2==false){
+       razmerX=10;
+       razmerY=10;
+       kvPx=600/razmerX;
+       for(let x=0; x<razmerX; x++){
+        tipVPole[x]=[]
+          for(let y=0; y<razmerY; y++ ){
+            tipVPole[x][y]=1+randomInteger(brTipove);
+          }
+       }
+       nivo2=true
     }
     while(klX-br1>=0 && tipVPole[klX-br1][klY]==tipVPole[klX][klY]){
         br1++;
